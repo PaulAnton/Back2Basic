a0830dd4bb447e58b31ab57881c9a248fdf10b5e
Arpit Nigam <arpit.nigam@outlook.com>
2016-07-11 21:17:50 -0700

    Palindrome with reverse and slice

---
+++ CODE/3-Palindrome.py
@@ -0,0 +1,13 @@
+def isPalindrome(aString):
+    '''
+    aString: a string
+    '''
+    # Your code here
+    n=len(aString)
+    if (n<=1):
+        return True
+    else:
+        for i in range((n+1)/2):
+            if aString[i]!=aString[n-i-1]:
+                return False
+        return True
