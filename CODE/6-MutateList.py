d699e55aefa329a72e506a262e8d10f69e1c0af7
Arpit Nigam <arpit.nigam@outlook.com>
2016-07-11 21:25:16 -0700

    Mutate the list on certain condition

---
+++ CODE/6-MutateList.py
@@ -0,0 +1,16 @@
+def satisfiesF(L):
+    """
+    Assumes L is a list of strings
+    Assume function f is already defined for you and it maps a string to a Boolean
+    Mutates L such that it contains all of the strings, s, originally in L such
+            that f(s) returns True, and no other elements. Remaining elements in L
+            should be in the same order.
+    Returns the length of L after mutation
+    """
+    # Your function implementation here
+    for i in range(len(L)-1,-1,-1):
+        if f(L[i]):
+            continue
+        else:
+            L.pop(i)
+    return len(L)
